3143d27053c2d4613a061a27ae47ab1bb757b370
XeniaWeb <xenia1898@gmail.com>
2026-01-06 17:59:54 +0700

    Замена бд на MySQL

---
+++ docker/mysql/init.sql
@@ -0,0 +1,6 @@
+-- Дополнительные настройки при инициализации
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+SET TIME_ZONE = '+00:00';
+SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
+SET FOREIGN_KEY_CHECKS = 1;
